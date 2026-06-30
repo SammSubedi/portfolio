c99b8f2ba74cb9687cbf6f2395a7e1ec65049227
SammSubedi <sank.subedi@gmail.com>
2026-06-30 12:19:44 +0545

    Connect Express to PostgreSQL with pg Pool and test route

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import pool from "./db.js";
 
 const app = express();
 const PORT = 5000;
@@ -7,6 +8,16 @@ app.get("/", (req, res) => {
     res.send("Server is running");
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on: http//localhost:${PORT}`);
+app.get("/test-db", async (req, res) => {
+    try{
+        const result = await pool.query('SELECT NOW()');
+        res.send(`Database connected. Current time: ${result.rows[0].now}`);
+    } catch (err){
+        console.error(err);
+        res.status(500).send('Database connection failed');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
 });
